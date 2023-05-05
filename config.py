5ffea4c9c811340cab51e9a5a1b616246b5ba231
Shenglau <96124506+mayuci@users.noreply.github.com>
2023-05-05 16:31:48 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "jdsportdatabase.cpmetbxdpldc.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "jdsports"
+custombucket = "jdssport-booking"
+customregion = "us-east-1"
+
